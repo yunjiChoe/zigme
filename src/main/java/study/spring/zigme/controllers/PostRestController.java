945ea960790a121a9f2db2b9f3d4124fef567dd8
taehyeon <taehyeon3657@gmail.com>
2021-07-13 14:28:58 +0900

    [김태현] 게시글 json형식으로 CRUD 완료

--- src/main/java/study/spring/zigme/controllers/PostRestController.java
+++ src/main/java/study/spring/zigme/controllers/PostRestController.java
@@ -155,13 +155,83 @@ public class PostRestController {
 	}
 	/**수정 폼에 대한 action 페이지 */
 	@RequestMapping(value= "/post", method = RequestMethod.PUT)
-	public Map<String, Object> put() {
-		return null;
+	public Map<String, Object> put(
+			@RequestParam(value="postNo", defaultValue="") int postNo,
+			@RequestParam(value="postSubtitle", defaultValue="") String postSubtitle,
+			@RequestParam(value="postTitle", defaultValue="") String postTitle,
+			@RequestParam(value="postContent", defaultValue="") String postContent,
+			@RequestParam(value="postUpcount", defaultValue="") int postUpcount,
+			@RequestParam(value="postViewcount", defaultValue="") int postViewcount,
+			@RequestParam(value="postRegdate", defaultValue="") String postRegdate,
+			@RequestParam(value="postNoti", defaultValue="") String postNoti,
+			@RequestParam(value="userNo", defaultValue="") int userNo) {
+		
+		/** 1)사용자가 입력한 파라미터 유효성 찾기 */
+		// 일반 문자열 입력 컬럼 --> String으로 파라미터가 선언되어 있는 경우는 값이 입력되지 않으면 빈 문자열로 처리된다.
+        if (!regexHelper.isValue(postSubtitle))     { return webHelper.getJsonWarning("소제목을 입력하세요."); }
+        if (!regexHelper.isValue(postTitle))     { return webHelper.getJsonWarning("제목을 입력하세요."); }
+        if (!regexHelper.isValue(postContent))     { return webHelper.getJsonWarning("내용을 입력하세요."); }
+        if (!regexHelper.isValue(postRegdate))     { return webHelper.getJsonWarning("게시날짜를 입력하세요."); }
+        if (!regexHelper.isValue(postNoti))     { return webHelper.getJsonWarning("알림여부를 입력하세요."); }
+        
+        // 숫자형으로 선언된 파라미터()
+        if (postUpcount < 0)                        { return webHelper.getJsonWarning("추천수는 0보다 작을 수 없습니다."); }
+        if (postViewcount < 0)                        { return webHelper.getJsonWarning("조회수는 0보다 작을 수 없습니다."); }
+        if (userNo == 0)                    { return webHelper.getJsonWarning("회원 일련 번호를 입력하세요."); }
+        
+		/** 2) 데이터 수정하기 */
+        //수정할 값들을 Beans에 담는다.
+        Post input = new Post();
+        input.setPostNo(postNo);
+		input.setPostSubtitle(postSubtitle);
+		input.setPostTitle(postTitle);
+		input.setPostContent(postContent);
+		input.setPostUpcount(postUpcount);
+		input.setPostViewcount(postViewcount);
+		input.setPostRegdate(postRegdate);
+		input.setPostNoti(postNoti);
+		input.setUserNo(userNo);
+		
+		//수정된 결과를 조회하기 위한 객체 생성
+		Post output = null;
+		
+		try {
+			//데이터 수정
+			postService.editPost(input);
+			//수정 결과 조회
+			output = postService.getPostItem(input);
+		} catch (Exception e) {
+			return webHelper.getJsonError(e.getLocalizedMessage());
+		}
+		
+		/** 3) 결과를 확인하기 위한 JSON출력 */
+		 Map<String, Object> map = new HashMap<String, Object>();
+	     map.put("item", output);
+	     return webHelper.getJsonData(map);
 	}
 	
 	/** 삭제 처리 */
 	@RequestMapping(value= "/post", method = RequestMethod.DELETE)
-	public Map<String, Object> delete() {
-		return null;
+	public Map<String, Object> delete(
+			@RequestParam(value="postNo", defaultValue = "0") int postNo) {
+		
+		/** 1) 파라미터 유효성 검사*/
+		if(postNo == 0) {
+			return webHelper.getJsonWarning("게스글번호가 없습니다.");
+		}
+		/** 2) 데이터 삭제하기 */
+		//데이터 삭제에 필요한 조건값을 Beans에 저장하기
+		Post input = new Post();
+		input.setPostNo(postNo);
+		
+		try {
+			postService.deletePost(input); //데이터 삭제
+		} catch (Exception e) {
+			return webHelper.getJsonError(e.getLocalizedMessage());
+		}
+		
+		/** 3) 결과를 확인하기 위한 JSON 출력*/
+		//확인할 대상이 삭제된 결과값만 OK로 전달
+		return webHelper.getJsonData();
 	}
 }
