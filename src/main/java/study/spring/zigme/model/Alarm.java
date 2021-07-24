de81c593b61c16caa30db6c3ad85d36cca7f8fd7
taehyeon <taehyeon3657@gmail.com>
2021-07-25 08:33:58 +0900

    [김태현] 알람테이블 단위테스트 완료

---
+++ src/main/java/study/spring/zigme/model/Alarm.java
@@ -0,0 +1,12 @@
+package study.spring.zigme.model;
+
+import lombok.Data;
+
+@Data
+public class Alarm {
+	private int alarmNo;
+	private String alarmAct;
+	private String alarmContent;
+	private String alarmTime;
+	private int userNo;
+}
